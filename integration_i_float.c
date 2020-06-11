ec4a5b1734b2e391d24c4c83e802f602cea6b36e
Sergeant-Salz <sprengstoff16@gmx.de>
2020-06-11 15:29:36 +0000

    Fixed calculation syntax in integrate

--- integration_i_float.c
+++ integration_i_float.c
@@ -12,13 +12,27 @@
 
 i_float integrate(i_float (*function)(i_float), i_float start, i_float stepSize, int steps){
     i_float sum = 0;
-    step_f = 0;
+    i_float step_f = 0;
     for(int step = 0; step < steps; step++){
         step_f = f_add(step_f, ONE);
         //sum += (function(start + step * stepSize) + function(start + (step + 1) * stepSize)) / 2;
-        sum = f_add(sum, f_add( 
-            function(f_add(start, f_mul(step_f, stepSize))),
-            f_mul(function(f_add(start, f_mul( f_add(step_f, ONE), stepSize))), ONE_HALF);
+        sum = f_add(sum, 
+            f_add( 
+                function(
+                    f_add(start, 
+                        f_mul(step_f, stepSize))),
+                f_mul(
+                    function(
+                        f_add(start, 
+                            f_mul(
+                                f_add(step_f, ONE),
+                                stepSize)
+                            )
+                        ),
+                    ONE_HALF
+                    )
+                )
+        );
     }
 
     return f_mul(sum, stepSize);
@@ -26,11 +40,16 @@ i_float integrate(i_float (*function)(i_float), i_float start, i_float stepSize,
 
 i_float myFunc(i_float x){
     // 2.2345f*x*x - 57*x
-    return  f_sub( f_mul(f_mul(FUNC_CONST1,x) x) , f_mul(FUNC_CONST2, x));
+    return f_sub(
+        f_mul(
+            f_mul(FUNC_CONST1,x), 
+            x),
+        f_mul(FUNC_CONST2, x)
+        );
 }
 
 int main(int argc, char** argv){
-    printf("Result is:"); pint_i_float(integrate(myFunc, START_VALUE, STEPSIZE, STEPS));
+    printf("Result is:"); print_i_float(integrate(myFunc, START_VALUE, STEPSIZE, STEPS));
     for(int i = 0; i < ITERATION_COUNT; i++){
         integrate(myFunc, START_VALUE, STEPSIZE, STEPS);
     }
