d32b21557e198ec588f00ac0e5fae6ce0762c7a7
Sergeant-Salz <sprengstoff16@gmx.de>
2020-06-12 15:02:57 +0000

    Changed integration function, fixed i_float integration, fixed fpu integrations calucaltion

--- integration_fpu.c
+++ integration_fpu.c
@@ -5,19 +5,23 @@ float integrate(float (*function)(float), float start, float stepSize, int steps
     float sum = 0;
 
     for(int step = 0; step < steps; step++){
-        if(DEBUG){printf("%f\n",sum);}
-        sum += (function(start + step * stepSize) + function(start + (step + 1) * stepSize)) / 2;
+        if(DEBUG){printf("%f\n",  sum);}
+        sum += function(start + step * stepSize) + (function(start + (step + 1) * stepSize)  * 0.5);
     }
 
     return sum * stepSize;
 }
 
+float quadratic(float x){
+    return x*x;
+}
+
 float myFunc(float x){
-    return 2.2345*x*x - 57*x;
+    return 15*x*x - 57*x;
 }
 
 int main(int argc, char** argv){
-    printf("Function at point 1 is:%f\n", myFunc(1));
+    printf("Function at x=1 is:%f\n", myFunc(1));
     printf("Result is:%f\n", integrate(myFunc, 1, 0.005, STEPS));
     for(int i = 0; i < ITERATION_COUNT; i++){
         integrate(myFunc, 1, 0.005, STEPS);
--- integration_i_float.c
+++ integration_i_float.c
@@ -7,9 +7,7 @@ i_float integrate(i_float (*function)(i_float), i_float start, i_float stepSize,
     i_float sum = 0;
     i_float step_f = 0;
     for(int step = 0; step < steps; step++){
-        step_f = f_add(step_f, ONE);
         if(DEBUG){print_i_float(sum);}
-        //sum += (function(start + step * stepSize) + function(start + (step + 1) * stepSize)) / 2;
         sum = f_add(sum, 
             f_add( 
                 function(
@@ -28,13 +26,18 @@ i_float integrate(i_float (*function)(i_float), i_float start, i_float stepSize,
                     )
                 )
         );
+        step_f = f_add(step_f, ONE);
     }
 
     return f_mul(sum, stepSize);
 }
 
+i_float quadratic(i_float x){
+    return f_mul(x,x);
+}
+
 i_float myFunc(i_float x){
-    // 2.2345f*x*x - 57*x
+    //15*x*x - 57*x
     return f_sub(
         f_mul(
             f_mul(FUNC_CONST1,x), 
@@ -44,8 +47,8 @@ i_float myFunc(i_float x){
 }
 
 int main(int argc, char** argv){
-    printf("Function at point 1 is:"); print_i_float(myFunc(START_VALUE));
-    //printf("Result is:"); print_i_float(integrate(myFunc, START_VALUE, STEPSIZE, STEPS));
+    printf("Function at x=1 is:\n"); print_i_float(myFunc(START_VALUE));
+    printf("Result is:\n"); print_i_float(integrate(myFunc, START_VALUE, STEPSIZE, STEPS));
     for(int i = 0; i < ITERATION_COUNT; i++){
         //integrate(myFunc, START_VALUE, STEPSIZE, STEPS);
     }
